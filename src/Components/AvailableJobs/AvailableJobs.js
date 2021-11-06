570a09bd6cb3810ed78e5da76fda7f2b3af128c0
Mequidi <nikhilsejwal234@gmail.com>
2021-11-07 01:54:02 +0530

    pagination done css left

--- src/Components/AvailableJobs/AvailableJobs.js
+++ src/Components/AvailableJobs/AvailableJobs.js
@@ -3,15 +3,46 @@ import { useState,useEffect } from "react";
 import JobApplicantsModal from "./JobApplicantsModal";
 import { getPostedJobs } from "../../Utilites/Api";
 import { Link } from "react-router-dom"; 
+import ReactPaginate from "react-paginate";
 
 const AvailableJobs = () =>{
 
     const [ selectedId,setSelectedId ] = useState("");
     const [ jobs,setJobs ] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+    const jobsPerPage = 8;
+    const jobsVisited = pageNumber * jobsPerPage;
+
+    const displayjobs = jobs
+    .slice(jobsVisited, jobsVisited + jobsPerPage)
+    .map((item) => {
+        console.log(item)
+            return <li key={item.id} className={styles["list-item"]}>
+                <h3>{item.title}</h3>
+                <p className={styles["list-description"]}>{item.description}</p>
+                <div className={styles["container"]}>
+                    <div className={styles["location-container"]}>
+                        <i></i>
+                        <p>{item.location}</p>
+                    </div>
+                    <button onClick={() =>{
+                        setShowModal(true);
+                        setSelectedId(item.id)
+                        console.log(item.id)
+                    }}>View Applications</button>   
+                </div>
+            </li>
+    });
+
+    const pageCount = Math.ceil(jobs.length / jobsPerPage);
+
+    const changePage = ({ selected }) => {
+      setPageNumber(selected);
+    };
+
     useEffect(()=>{
         getPostedJobs()
         .then((result)=>{
-            console.log(result)
             setJobs(result);
         });
     },[]);
@@ -19,7 +50,7 @@ const AvailableJobs = () =>{
     const closeBtnClickHandler = () =>{
         setShowModal(false)
     }
-    console.log(jobs)
+    
     return <div className={styles.jobs}>
         {showModal && <JobApplicantsModal id={selectedId} onClick={closeBtnClickHandler}></JobApplicantsModal>}
         <p className={styles["navigation"]}>
@@ -28,23 +59,7 @@ const AvailableJobs = () =>{
         </p>
         <h2 className={styles["heading"]}>Jobs posted by you</h2>
         {jobs?<ul className={styles["job-list"]}>
-            {jobs.map((item)=>{
-                return <li key={item.id} className={styles["list-item"]}>
-                    <h3>{item.title}</h3>
-                    <p className={styles["list-description"]}>{item.description}</p>
-                    <div className={styles["container"]}>
-                        <div className={styles["location-container"]}>
-                            <i></i>
-                            <p>{item.location}</p>
-                        </div>
-                        <button onClick={() =>{
-                            setShowModal(true);
-                            setSelectedId(item.id)
-                            console.log(item.id)
-                        }}>View Applications</button>   
-                    </div>
-                </li>
-            })}
+        {displayjobs}
         </ul>:
         <div className={styles["no-post"]}> 
             <div>
@@ -54,7 +69,17 @@ const AvailableJobs = () =>{
             </div>  
         </div>}
         {/* pagination nav */}
-        {<div></div>}
+        {<ReactPaginate
+            previousLabel={"<"}
+            nextLabel={">"}
+            pageCount={pageCount}
+            onPageChange={changePage}
+            containerClassName={"paginationBtns"}
+            previousLinkClassName={"previousBtn"}
+            nextLinkClassName={"nextBtn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+      />}
     </div>
 }
 
